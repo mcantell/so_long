2037097625d8657d9502642ebfb4d6732f802646
Mario Cantelli <mcantell@e3r4p6.42roma.it>
2024-05-22 12:34:35 +0200

    controlli mappa 100%

--- check_path.c
+++ check_path.c
@@ -6,7 +6,7 @@
 /*   By: mcantell <mcantell@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 03:39:48 by mcantell          #+#    #+#             */
-/*   Updated: 2024/05/19 05:43:16 by mcantell         ###   ########.fr       */
+/*   Updated: 2024/05/22 12:33:51 by mcantell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 void	fill(char**tab, int size, int column, t_coord cur)
 {
 	if (cur.colum < 0 || cur.colum >= column || cur.side < 0
-		|| cur.side >= size || tab[cur.colum][cur.side] != '1'
-		|| tab[cur.colum][cur.side] != 'F')
+		|| cur.side >= size || tab[cur.side][cur.colum] == '1'
+		|| tab[cur.side][cur.colum] == 'F')
 		return ;
-	tab[cur.colum][cur.side] = 'F';
+	tab[cur.side][cur.colum] = 'F';
 	fill(tab, size, column, (t_coord){cur.side - 1, cur.colum});
 	fill(tab, size, column, (t_coord){cur.side + 1, cur.colum});
 	fill(tab, size, column, (t_coord){cur.side, cur.colum - 1});
@@ -33,11 +33,10 @@ int	count(t_game *smap, char c)
 
 	i = 1;
 	index = 0;
-
-	while (smap->cmap[i])
+	while (smap->cmap[i] != NULL)
 	{
 		t = 1;
-		while (smap->cmap[i][t])
+		while (smap->cmap[i][t] != '\0')
 		{
 			if (smap->cmap[i][t] == c)
 				index++;
@@ -57,7 +56,7 @@ int	p_line(t_game *smap, char c)
 	while (smap->map[i])
 	{
 		t = 1;
-		while (smap->map[i][t])
+		while (smap->map[i][t] != '\0')
 		{
 			if (smap->map[i][t] == c)
 				return (i);
@@ -85,23 +84,17 @@ int	p_column(t_game *smap, char c, int i)
 void	check_path(t_game *smap)
 {
 	t_coord	p_c;
-	int		i;
 	int		c;
 	int		e;
 
-	i = 1;
 	c = 0;
 	e = 0;
 	p_c.side = p_line(smap, 'P');
 	p_c.colum = p_column(smap, 'P', p_c.side);
 	smap->cmap = smap->map;
-	fill(smap->cmap, matrix_len(smap), ft_strlen(smap->cmap[0]), p_c);
-	while (smap->cmap[i])
-	{
-		e += count(&smap[i], 'E');
-		c += count (&smap[i], 'C');
-		i++;
-	}
+	fill(smap->cmap, matrix_len(smap) - 1, ft_strlen(smap->cmap[0]) - 1, p_c);
+	e += count(smap, 'E');
+	c += count (smap, 'C');
 	if (c != 0 || e != 0)
 		exit (write (2, "Error\nroute not avaiable\n", 26));
 }
